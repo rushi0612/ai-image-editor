8c50651b0aa279444c40c734c5b1bdcdad0985d5
Rushikesh <rvp6123@gmail.com>
2026-01-17 11:14:39 +0530

    Add Header component with logo, nav links, and auth placeholder

---
+++ ai-image-editor/src/components/Header.jsx
@@ -0,0 +1,36 @@
+'use client'
+import Image from "next/image";
+import Link from 'next/link';
+import { usePathname } from "next/navigation";
+
+const Header = () => {
+  const path = usePathname()
+  return (
+    <header className="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 text-nowrap">
+       <div className="background-blur-md bg-white/10 border-white/20 rounded-full px-8 py-3 flex items-center justify-between">
+       <Link href='/' className="mr-10 md:mr-20">
+        <Image src="/logo-text.png" alt="Pixxel Logo" className="min-w-24 object-cover" width={96} height={24} />
+       </Link>
+       {path === "/" && (<div className="hidden md:flex space-x-6">
+        <Link href="#features"
+         className="text-white font-medium transition-all duration-300 hover:text-cyan-400 cursor-pointer">
+        Features
+        </Link>
+        <Link href="#prising"
+         className="text-white font-medium transition-all duration-300 hover:text-cyan-400 cursor-pointer">
+        Prising
+        </Link>
+        <Link href="#contact"
+         className="text-white font-medium transition-all duration-300 hover:text-cyan-400 cursor-pointer">
+        Contact
+        </Link>
+        </div> )}
+
+        <div className="flex items-center gap-3 ml-10 md:ml-20">auth</div>
+
+       </div> 
+    </header>
+  )
+}
+
+export default Header   
