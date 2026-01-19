3e3a823f40679bc773899050e9c9dec6998b01b0
Rushikesh <rvp6123@gmail.com>
2026-01-19 13:41:05 +0530

    Refactor auth pages to use Clerk authentication

---
+++ app/(auth)/layout.js
@@ -0,0 +1,10 @@
+'use client';
+
+
+const AuthLayout = ({children}) => {
+  return (
+    <div className="flex min-h-screen items-center justify-center  pt-48">{children}</div>
+  )
+}
+
+export default AuthLayout
